b0eb0d00cce32e450422ef3b134f489396d3fb47
Razrector <komar86311@gmail.com>
2019-12-07 23:57:18 +0100

    Added database support

--- src/main/java/RecordDB/App.java
+++ src/main/java/RecordDB/App.java
@@ -1,5 +1,9 @@
 package RecordDB;
 
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 public class App {
     public static void main (String[] args){
         System.out.println("Hello World!");
