38b09a19f3306ad825cc9818e69fc3b354eb44b6
Marcin Kostrzewski <komar86311@gmail.com>
2019-12-11 23:11:34 +0100

    Redone queries

--- src/main/java/Database/Queries/Queries.java
+++ src/main/java/Database/Queries/Queries.java
@@ -1,6 +1,9 @@
 package Database.Queries;
 
-import Database.Model.*;
+import Database.Model.Album;
+import Database.Model.Artist;
+import Database.Model.Genre;
+import Database.Model.Song;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -14,40 +17,40 @@ public class Queries {
         this.entityManager = entityManager;
     }
 
-    public List<Album> getAlbumsFromArtist(Artist artist){
+    public List<Album> getAlbumsFromArtist(Artist artist) {
         TypedQuery<Album> query = entityManager.createQuery(
                 "SELECT a FROM Album a WHERE a.artist = :artist ORDER BY a.name ASC", Album.class);
         return query.setParameter("artist", artist).getResultList();
     }
 
-    public List<Song> getSongsOnAlbum(Album album){
+    public List<Song> getSongsOnAlbum(Album album) {
         TypedQuery<Song> query = entityManager.createQuery(
-                "SELECT s FROM Song s WHERE s.album = :album  ORDER BY s.disc ASC, s.number ASC", Song.class);
+                "SELECT s FROM Song s WHERE s.album = :album ORDER BY s.disc ASC, s.number ASC", Song.class);
         return query.setParameter("album", album).getResultList();
     }
 
-    public List<Album> getAllAlbumsByGenre(Genre genre, int pageNr){
+    public List<Album> getAllAlbumsByGenre(Genre genre, int pageNr) {
         Query queryTotal = entityManager.createQuery("SELECT COUNT(a) FROM Album a WHERE a.genre = :genre ");
-        long countResult = (long)queryTotal.setParameter("genre", genre).getSingleResult();
+        long countResult = (long) queryTotal.setParameter("genre", genre).getSingleResult();
         Query query = entityManager.createQuery("SELECT a FROM Album a WHERE a.genre = :genre").setParameter("genre", genre);
         int pageSize = 10;
         int pageNumber = (int) ((countResult / pageSize) + 1);
         if (pageNr > pageNumber) pageNr = pageNumber;
-        query.setFirstResult((pageNr-1) * pageSize);
+        query.setFirstResult((pageNr - 1) * pageSize);
         query.setMaxResults(pageSize);
 
         return query.getResultList();
     }
 
-    public List<Release> getAlbumReleases(Album album){
-        TypedQuery<Release> query = entityManager.createQuery(
-                "SELECT r FROM Release r WHERE r.album = :album", Release.class);
-        return query.setParameter("album", album).getResultList();
+    public Artist getArtistByName(String artistName) {
+        TypedQuery<Artist> query = entityManager.createQuery(
+                "SELECT a FROM Artist a WHERE a.name = :name", Artist.class);
+        return query.setParameter("name", artistName).getSingleResult();
     }
 
-    public List<Artist> getLabelArtists(Label label){
+    public List<Artist> getArtistGenres(Artist artist) {
         TypedQuery<Artist> query = entityManager.createQuery(
-                "SELECT a FROM Artist a WHERE :label MEMBER OF a.labels ", Artist.class);
-        return query.setParameter("label", label).getResultList();
+                "SELECT DISTINCT g FROM Artist ar INNER JOIN Album a ON a.artist = :artist INNER JOIN Genre g ON a.genre = g", Artist.class);
+        return query.setParameter("artist", artist).getResultList();
     }
 }
