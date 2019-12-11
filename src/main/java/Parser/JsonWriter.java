4354d1a069c629a9c306035b33e5389b8e726518
Marcin Kostrzewski <komar86311@gmail.com>
2019-12-11 23:11:57 +0100

    No longer prints null values

--- src/main/java/Parser/JsonWriter.java
+++ src/main/java/Parser/JsonWriter.java
@@ -1,7 +1,10 @@
 package Parser;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 import java.io.File;
@@ -13,6 +16,8 @@ public class JsonWriter<T> {
 
     public void serializeList(String filepath, List<T> objects) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.registerModule(new JodaModule());
         objectMapper.writeValue(new File(filepath), objects);
@@ -20,6 +25,8 @@ public class JsonWriter<T> {
 
     public void serialize(String filepath, T object) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.writeValue(new File(filepath), object);
     }
